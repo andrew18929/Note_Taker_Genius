b6774fd49835c1e4d208f75223283592132e2123
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-12 12:29:40 -0500

    coded HTMLRoutes.js

--- routes/HTMLRoutes.js
+++ routes/HTMLRoutes.js
@@ -4,3 +4,12 @@ const fs = require('fs');
 const path = require('path');
 
 // this is where I would add module.exports. Need to figure out how to code it
+module.exports = function(app) {
+    app.get('/api/notes', function (req, res) {
+        res.sendFile(path.join(__dirname, 'public/notes.html'));
+    });
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+    });
+};
