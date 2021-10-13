18ad5d1c25891d2990f9b16c3135f6acee636e24
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-13 12:10:08 -0500

    updated server.js

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/HTMLRoutes')(app);
+//require('./routes/HTMLRoutes')(app);
+const htmlRoutes = require('./routes/htmlRoutes');
+app.use('/', htmlRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../develop/public/index.html'));
+});
 
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
